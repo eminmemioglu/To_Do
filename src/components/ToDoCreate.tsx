39fa68ca3720426b43b610db6474b6b9d9d06d88
eminmemioglu <muhammed.memioglu12@gmail.com>
2025-03-12 13:58:30 +0300

    css

---
+++ src/components/ToDoCreate.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ToDoCreate() {
+  return (
+    <div>
+      <input type="text" />
+      <button>To Do ekle</button>
+    </div>
+  )
+}
+
+export default ToDoCreate
